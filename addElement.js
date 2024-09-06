60b944f4fb7cef87807da49619a4873ee8659410
An-te <179237615+An-te@users.noreply.github.com>
2024-09-06 13:29:45 +0900

    add

--- addElement.js
+++ addElement.js
@@ -13,6 +13,9 @@ function addElement() {
 			case "img":
 				addImg(img);
 				break;
+			case "txt":
+				addTxt(text);
+				break;
 			default:
 		}
 	}
@@ -84,6 +87,16 @@ function addImg(img) {
 	newContent.loading = "lazy";
 	newDiv.appendChild(newContent);
 
+	const currentDiv = document.getElementById("parent");
+	currentDiv.appendChild(newDiv);
+}
+
+function addTxt(text) {
+	const newDiv = document.createElement("div");
+	newDiv.className = "child";
+	const newContent = document.createElement("span");
+	newContent.textContent = text;
+	newDiv.appendChild(newContent);
 	const currentDiv = document.getElementById("parent");
 	currentDiv.appendChild(newDiv);
 }
