2bb2c1f59729984623434d817f339655b15b742d
An-te <179237615+An-te@users.noreply.github.com>
2024-09-28 15:04:20 +0900

    ref

--- addElement.js
+++ addElement.js
@@ -1,5 +1,4 @@
 import Sketch from "./sketch/sketch.js";
-import cssAnimation from "./cssAnimation.js";
 import createP5 from "./js/createP5.js";
 import createImg from "./js/createImg.js";
 import createTxt from "./js/createTxt.js";
@@ -8,10 +7,14 @@ document.body.onload = addElement;
 
 function addElement() {
 	p5.prototype.shuffle(Sketch, true);
+
+	const obAnimation = keyFrameAnimation("p5Anim");
+	obAnimation.css.map((e) => document.styleSheets[0].insertRule(e));
+
 	for (const [i, { func, t, text, img, type }] of Sketch.entries()) {
 		switch (type) {
 			case "p5js":
-				createP5(`p5Sketch${i}`, func, t, text);
+				createP5(`p5Sketch${i}`, func, t, text, obAnimation.name);
 				break;
 			case "img":
 				createImg(img);
@@ -22,10 +25,24 @@ function addElement() {
 			default:
 		}
 	}
+}
 
-	let anim = cssAnimation().keyframe;
-	anim = p5.prototype.shuffle(anim)[0];
-	document.styleSheets[0].insertRule(anim);
-	// var div = document.getElementById('anim');
-	// div.style.animation = 'anim 1s linear forwards';
+function keyFrameAnimation(cls) {
+	const k = [
+		"from { filter: blur(10px) invert(100%); } to { filter: blur(0px); }",
+		"from { filter: blur(10px); } to { filter: blur(0px) invert(100%); }",
+		"from { filter: blur(10px); } ",
+	];
+
+	const css = (n, k) => [
+		`.${n} { animation: ${n} 3s forwards; }`,
+		`@keyframes ${n} { ${k} }`,
+	];
+
+	const keyframe = p5.prototype.shuffle(k)[0];
+
+	return {
+		name: cls,
+		css: css(cls, keyframe),
+	};
 }
